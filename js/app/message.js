4f7f9abd910c70bf2ebfff47132322dd169d6797
Ryan Dewell <ryan@dewell.org>
2018-04-20 11:27:44 -0600

    Simplify messages code using documentElement

--- js/app/message.js
+++ js/app/message.js
@@ -16,7 +16,7 @@
         var messages = document.createElement('div');
         messages.id = MESSAGES_ID;
         messages.setAttribute('class', MESSAGES_CLASSES);
-        document.body.appendChild(messages);
+        document.documentElement.appendChild(messages); // documentElement allows Turbolinks to swap out the body.
         // TODO could look for an initial message in a meta element.
     }
 
@@ -27,22 +27,6 @@
             document.dispatchEvent(new CustomEvent(EVT, {detail: detail}));
         }
     };
-
-    // Carry over existing div to next body
-    // Similar idea to turbolinks permanent option
-    // Use cloneNode so no chance to see it removed from existing/current document.
-    // This appears to be fired for cached pages too (good)
-    if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
-        document.addEventListener('turbolinks:before-render', function (e) {
-            var existing = e.data.newBody.getElementsByClassName('messages'); // can't use ID lookup on an Element
-            if (existing.length > 0) existing[0].remove();
-
-            var messages = document.getElementById(MESSAGES_ID);
-            var clone = messages.cloneNode(true);
-
-            e.data.newBody.appendChild(clone);
-        });
-    }
 
     // Ignores any event data that doesn't look like a message
     document.addEventListener(EVT, function(e){
@@ -66,26 +50,23 @@
 
         if (txt.length == 0) return;
 
-        var uniqid = String.fromCharCode(65 + Math.floor(Math.random() * 26)) + Date.now();
-
         var newMessage = document.createElement('div');
-        newMessage.id = uniqid;
         newMessage.innerText = txt;
         newMessage.setAttribute('class', MESSAGE_CLASSES + ' ' + cls);
 
         var messages = document.getElementById(MESSAGES_ID);
         messages.appendChild(newMessage);
 
-        // Must occur async, evidently, otherwise the animation won't show.
+        // Must occur async, otherwise the animation won't show.
         setTimeout(function(){
             newMessage.setAttribute('class', MESSAGE_CLASSES + ' message-show '  + cls);
         }, 1);
 
         setTimeout(function(){
             // Must lookup element in case we've cloned it after a redirect
-            document.getElementById(uniqid).setAttribute('class', MESSAGE_CLASSES + ' message-hide ' + cls);
+            newMessage.setAttribute('class', MESSAGE_CLASSES + ' message-hide ' + cls);
             // Must be greater than CSS hide transition
-            setTimeout(function(){document.getElementById(uniqid).remove()}, 300);
+            setTimeout(function(){newMessage.remove()}, 300);
         }, AUTO_HIDE_MS);
 
         if (e.detail.track == true){
