478d4b737608ffa7e06eb5b71f92240074ed901c
Ryan Dewell <ryan@dewell.org>
2019-01-14 11:00:22 -0700

    Copy project improvements

--- projects/{project}/copy/📮copy.js
+++ projects/{project}/copy/📮copy.js
@@ -1,47 +1,50 @@
 
 import {project} from '🔗';
-import {branch} from 'form';
+import {ref, newName} from 'form';
 import {Source} from '🔌';
 import {Me} from '👤';
 import {Project, ProjectUser, ProjectKey} from '📦';
+import {Identicon} from 'util';
+
+if (!ref || ref.length == 0) throw Messages.fieldError('ref', 'Choose a branch/tag');
+if (!newName || newName.length == 0) throw Messages.fieldError('newName', 'New project name is required');
 
 let current = project.get();
 
-let newBranch = branch;
-
-if (!branch) newBranch = current.source.branch;
-
 let projectCopy = new Project()
-    .source(current.source.withBranch(newBranch))
+    .icon(Identicon.random())
+    .source(current.source.withReferenceValue(ref))
     .properties(current.properties);
 
-projectCopy.name = projectCopy.source.name;
+projectCopy.name = newName;
 
 projectCopy(()=>{
-    let pu = new ProjectUser().user(Me);
-    let currentParent = current(()=>ProjectUser.me().get()).parent;
-    if (!currentParent.linked()){
+    /*let pu =*/ new ProjectUser().user(Me);
+
+    // No nesting for now
+    //let currentParent = current(()=>ProjectUser.me().get()).parent;
+    //if (!currentParent.linked()){
         /**
          * Nest
          */
-        pu.parent = current;
-    } else {
+     //   pu.parent = current;
+    //} else {
         /**
          * We don't currently allow more than one layer of nesting, so if you try to copy
          * an already nested project, it simply ends up under the same parent.
          */
-        pu.parent = currentParent;
-    }
+     //   pu.parent = currentParent;
+    // }
 });
 
 /**
- * Copy ProjectKey's to the new parent / new Project copy.
+ * Copy current ProjectKey's to the new Project copy.
  */
 ProjectKey.all().modify((key)=>{
     projectCopy(()=>key.copy());
 });
 
 /**
- * Back to 'parent'.
+ *
  */
-({redirect: `/projects/${current.id}/`, success: 'Copy created.'});
+({redirect: Redirect.dir('projects').dir(projectCopy.id).success('Copy created')});
