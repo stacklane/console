83c078858c17f356214206e4f78e54b6c62a6df1
Ryan Dewell <ryan@dewell.org>
2019-04-02 09:19:59 -0600

    Allow placeholder to reflect input color

--- js/app/reflect-input-color.js
+++ js/app/reflect-input-color.js
@@ -7,26 +7,19 @@
 
     App.register("reflect-input-color", class extends Stimulus.Controller {
         connect(){
-            var canvas = this.element.getElementsByTagName("canvas")[0];
-
-            // use fixed size
-           // canvas.width=canvas.parentElement.clientWidth;
-           // canvas.height=canvas.parentElement.clientHeight;
-
             this.update();
 
             var input = this.element.getElementsByTagName("input")[0];
             var thiz = this;
-            input.addEventListener('change', function(){
-                thiz.update();
-            });
+            input.addEventListener('change', function(){ thiz.update(); });
         }
         update(){
             var input = this.element.getElementsByTagName("input")[0];
-            var canvas = this.element.getElementsByTagName("canvas")[0];
-            var ctx = canvas.getContext("2d");
-            ctx.fillStyle = input.value;
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            var color = input.value && input.value.length ? input.value : input.placeholder;
+            if (color && color.length) {
+                var canvas = this.element.getElementsByTagName("canvas")[0];
+                canvas.getContext("2d").fillRect(0, 0, canvas.width, canvas.height);
+            }
         }
     });
 })(window);
