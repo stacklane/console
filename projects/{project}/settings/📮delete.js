edff0e679288b350d5962bcf362f3254ab44f45f
Ryan Dewell <ryan@dewell.org>
2019-09-19 07:11:17 -0600

    Remove deprecated

--- projects/{project}/settings/📮delete.js
+++ projects/{project}/settings/📮delete.js
@@ -1,9 +1,9 @@
 import {project} from '🔗';
 import {ProjectUser} from '📦';
 
-if (project.get().mapping){
-    throw Messages.error('Projects with domain mappings may not be deleted.');
-}
+//if (project.get().mapping){
+//    throw Messages.error('Projects with domain mappings may not be deleted.');
+//}
 
 if (ProjectUser.me().isSharing()){
 
@@ -21,7 +21,7 @@ if (ProjectUser.me().isSharing()){
      * Deleting unshared Project.
      */
 
-   // can't do this right now:  project.get().remove(); // Delete first, while 'me' still has access to it.
+    // can't do this right now:  project.get().remove(); // Delete first, while 'me' still has access to it.
     ProjectUser.me().get().remove(); // This would cascade async, but we want to delete it sync, so explicit remove().
 
     ({redirect: Redirect.home().success('Deleted Project')});
