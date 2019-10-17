7e27d24fb7c9f6e650438a744373437cd40774ca
Ryan Dewell <ryan@dewell.org>
2019-10-17 13:56:13 -0600

    Design tweaks

--- js/app/dropdown.js
+++ js/app/dropdown.js
@@ -1,14 +1,19 @@
 /**
- * For standard Bulma CSS structure.
+ * Use .is-dropdown-activator to manage the toggle of .is-active on the element this controller is attached to.
  */
 (()=>{
     'use strict';
-
     App.register("dropdown", class extends Stimulus.Controller {
         connect(){
-            var dropdown = this.element;
-            var link = dropdown.querySelector('.navbar-link');
-            link.addEventListener('click', function(){ dropdown.classList.toggle('is-active'); });
+            let dropdown = this.element;
+            let link = dropdown.querySelector('.is-dropdown-activator');
+            link.addEventListener('click', ()=>{
+                dropdown.classList.toggle('is-active');
+                // Remove any others which are currently open:
+                document.querySelectorAll('.has-dropdown.is-active').forEach((e)=>{
+                   if (!e.isEqualNode(dropdown)) e.classList.toggle('is-active', false);
+                });
+            });
         }
     })
 })();
