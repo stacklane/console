75b48d64bd1724075ad09a0aa91869204863c140
Ryan Dewell <ryan@dewell.org>
2020-05-06 07:33:39 -0600

    Add Stacklane as source type

--- projects/{project}/{version}/📤/VersionSourceInfo.js
+++ projects/{project}/{version}/📤/VersionSourceInfo.js
@@ -3,10 +3,11 @@ import {version} from '🔗';
 let source = version.get().source;
 let exists = source.exists();
 
-let type = {github: false, bitbucket: false}
+let type = {github: false, bitbucket: false, stacklane: false}
 
 if (source.type == 'GitHub') type.github = true;
 else if (source.type == 'Bitbucket') type.bitbucket = true;
+else if (source.type == 'Stacklane') type.stacklane = true;
 
 let notExists = !exists;
 let existsCC = 0;
